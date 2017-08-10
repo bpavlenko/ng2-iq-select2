148ec7add5248d40178e9f6f2fec1a8fde4bec86
pavlenko.bohd <pavlenko.bohd@gmail.com>
2017-08-10 09:11:35 +0300

    delete old build

--- publish/src/app/iq-select2/iq-select2-item.d.ts
+++
@@ -1,5 +0,0 @@
-export interface IqSelect2Item {
-    id: string;
-    text: string;
-    entity?: any;
-}
--- src/app/iq-select2/messages.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Messages {
-    static readonly PARTIAL_COUNT_VAR: string;
-    static readonly TOTAL_COUNT_VAR: string;
-    moreResultsAvailableMsg?: string;
-    noResultsAvailableMsg?: string;
-}
